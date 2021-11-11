385ba5bb5efd2827e41da7e263c16707a4172683
rainbowzhouj <1004385861@qq.com>
2021-11-11 14:31:59 +0800

    字符串练习巩固

---
+++ 字符串/有效的字母异位词.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/11/11 11:29 上午
+# @Author  : rainbowzhouj
+# @FileName: 有效的字母异位词.py
+# @Software: PyCharm
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        """
+        将s与t 放到两个字典中，key为每个字母，value为每个字母出现的次数
+        将字典转化为列表后，比较两个列表中的值是否一样，一样则返回truw
+        :param s:
+        :param t:
+        :return:
+        """
+        y = {}
+        z={}
+        while len(s) == len(t):
+            for x in s:
+                if x not in y:
+                    y.update({x: s.count(x)})
+            for a in t:
+                if a not in z:
+                    z.update({a: t.count(a)})
+            if y.items() == z.items():
+                return True
+            else:
+                return False
+        return False
+
+if __name__ == '__main__':
+    s = "anagram"
+    t = "nagaran"
+    print(Solution.isAnagram(1, s, t))
