818b86fe612cb4123feab4944f0d70c0f15fec68
rainbowzhouj <1004385861@qq.com>
2022-07-25 16:04:49 +0800

    LCS 学习-滑动窗口

--- 字符串/最长公共前缀.py
+++ 字符串/最长公共前缀.py
@@ -27,5 +27,20 @@ class Solution:
         return pre
 
 if __name__ == '__main__':
-    strs = ["flower", "flow", "flight"]
+    strs = ["flower", "low"]
     print(Solution.longestCommonPrefix(1, strs))
+    # s="【卓越好课节】3＋1卓越方法课-剑桥高材生们都在用的『康奈尔笔记法』，让你学习知识轻松又快乐！（初一至高三）"
+    # print(len(s))
+
+    """
+    # 找出 2022年订单金额大于1的所有客户,在2022年按天级别的订单消费情况
+    select 订单id from 订单表 where  会员id=(
+    select distinct 会员id from 订单表 where 订单金额 > 100 and datatime 
+    )  and order_by datatime between dateadd(DAY,-365,2022-12-31);
+    
+    SELECT account_id,create_time FROM facebook_ads_insights WHERE account_id=(
+SELECT account_id FROM facebook_ads_insights WHERE spend>1 AND YEAR(create_time)=2022 )
+GROUP BY create_time BETWEEN '2022-01-01' and '2022-12-31';
+
+    """
+
---
+++ 字符串/最长公共子串.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/7 8:31 下午
+# @Author  : rainbowzhouj
+# @FileName: 最长公共子串.py
+# @Software: PyCharm
+# 两个字符串的，最大公共字串
+
+def twoString(a: str, b: str):
+    left = 0
+    res = ""
+    for right in range(len(a) + 1):
+        sub = a[right - left:right]
+        if sub in b:
+            res = sub
+            left += 1
+    return res
+
+
+def LCS(str1, str2):
+    m = len(str1)
+    n = len(str2)
+    dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+    pos = 0
+    max_len = 0
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i - 1] == str2[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+                if dp[i][j] > max_len:
+                    max_len = dp[i][j]
+                    pos = i - 1
+    res = str1[pos - max_len + 1:pos + 1]
+    return res
+
+
+if __name__ == '__main__':
+    print(twoString("hello", "eell"))
